80bf6f44eb0a841d9212aed14d9344600e4af133
malant <31500038+malant@users.noreply.github.com>
2022-06-07 15:07:05 +0100

    added sign in component as well as firebase utils and brought in firebase libraries

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,72 @@
+import { initializeApp } from 'firebase/app';
+import { 
+  getAuth, 
+  signInWithRedirect, 
+  signInWithPopup, 
+  GoogleAuthProvider 
+} from 'firebase/auth';
+
+
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from 'firebase/firestore'
+
+
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCiB1ukNDGQSAuKBG4RMBtPfjfqSPlKfyU",
+  authDomain: "crwn-db-ed0d9.firebaseapp.com",
+  databaseURL: "https://crwn-db-ed0d9.firebaseio.com",
+  projectId: "crwn-db-ed0d9",
+  storageBucket: "crwn-db-ed0d9.appspot.com",
+  messagingSenderId: "995172397453",
+  appId: "1:995172397453:web:47044f8ff6e15bc9"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+if(!userSnapshot.exists()) {
+  const { displayName, email } = userAuth;
+  const createdAt = new Date();
+
+  try {
+    await setDoc(userDocRef, {
+      displayName,
+      email,
+      createdAt
+    });
+  } catch(error) {
+    console.log('Error creating the user', error.message);
+  }
+}
+
+return userDocRef;
+ 
+}
